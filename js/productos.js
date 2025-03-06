258b2689423f0bd66682cda8929054b864fce9cc
josue Huayapa <huayapajosue0@gmail.com>
2025-03-05 19:39:51 -0500

    component card offert

--- js/productos.js
+++ js/productos.js
@@ -1,3 +1,5 @@
-document.addEventListener("DOMContentLoaded", e => {
+import { sliderCardOffert } from "./functions/sliderCard.js";
 
+document.addEventListener("DOMContentLoaded", e => {
+  sliderCardOffert();
 });
